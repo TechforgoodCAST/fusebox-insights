3e525ba341d6aa9f3656a81408d3094d9366de8a
Ian Edwards <ianedwards559@gmail.com>
2019-02-25 16:18:59 +0000

    can still add insight properly with type attribute, add form element

--- app/models/response.rb
+++ app/models/response.rb
@@ -7,26 +7,39 @@ class Response
     'No change' => 0, 'More confident' => 1, 'Less confident' => -1
   }.freeze
 
-  attr_accessor :author, :confidence, :description, :title, :unknown
+  TYPE = {
+    'Insight' => 'insight', 'Comment' => 'comment'
+  }.freeze
 
-  validates :author, :confidence, :title, :unknown, presence: true
+
+  attr_accessor :author, :confidence, :description, :title, :unknown, :type
+
+  validates :author, :unknown, presence: true
 
   def valid?
-    if Insight.where(title: title).any?
-      super
-      errors.add(:title, 'has already been taken')
-      false
-    else
+    if type == TYPE['Insight']
+      if Insight.where(title: title).any?
+        super
+        errors.add(:title, 'has already been taken')
+        false
+      else
+        super
+      end
+    elsif type == TYPE['Comment']
       super
     end
   end
 
   def save
     if valid?
-      insight = author.insights.create!(title: title, description: description)
-      author.proofs.create!(
-        confidence: confidence, insight: insight, unknown: unknown
-      )
+      if type == TYPE['Insight']
+        insight = author.insights.create!(title: title, description: description)
+        author.proofs.create!(
+          confidence: confidence, insight: insight, unknown: unknown
+        )
+      elsif type == TYPE['Comment']
+        false
+      end
     else
       false
     end
