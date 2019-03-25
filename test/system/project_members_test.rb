3618d002cbc9e4100526387702af6aec73c2499e
Richard Smith <richardsmith@dev.ngo>
2019-03-25 06:52:31 +0000

    project members system test

---
+++ test/system/project_members_test.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'application_system_test_case'
+
+class ProjectMembersTest < ApplicationSystemTestCase
+
+  setup do
+  end
+
+  test 'show index page' do
+  end
+
+  test 'create member' do
+  end
+
+  test 'destroy member' do
+  end
+
+  test 'non-owner access denied for create' do
+  end
+  
+  test 'non-owner access denied for destroy' do
+  end
+   
+end
