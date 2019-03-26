a6102c1b7882e5ac4c655aa932e2eed36663aba0
Richard Smith <richardsmith@dev.ngo>
2019-03-26 09:49:27 +0000

    controller and tests

--- app/controllers/support_messages_controller.rb
+++ app/controllers/support_messages_controller.rb
@@ -2,9 +2,9 @@
 class SupportMessagesController < ApplicationController
   before_action :authenticate_user!
   before_action :set_support_message
+  before_action :set_project
   
   def index
-    @project = Project.find_by(slug: params[:project_slug])
     @support_messages = @project.support_messages.order(:order)
   end
 
@@ -13,16 +13,18 @@ class SupportMessagesController < ApplicationController
   end
   
   def new
+    if !is_user_project_creator
+      redirect_to support_messages_path, notice: 'You do not have perission to create support messages.'
+    end
     @support_message = SupportMessage.new
   end
   
   def create
-    @current_project = Project.find_by(slug: params[:project_slug])
-    @new_message = SupportMessage.new(support_message_params)
-    @new_message.status = 'Pending'
-    @new_message.project = @current_project
-    if @new_message.save
-      redirect_to support_message_path, notice: 'Support message created successfully.'
+    @support_message = SupportMessage.new(support_message_params)
+    @support_message.status = 'Pending'
+    @support_message.project = @project
+    if @support_message.save
+      redirect_to support_messages_path, notice: 'Support message created successfully.'
     else
       render :new
     end
@@ -35,7 +37,7 @@ class SupportMessagesController < ApplicationController
   def update
     authorize @support_message
     if @support_message.update(support_message_params)
-      redirect_to edit_support_message_path, notice: 'Support message updated successfully.'
+      redirect_to support_messages_path(slug: @support_message.project.slug), notice: 'Support message updated successfully.'
     else
       render :edit
     end
@@ -44,13 +46,21 @@ class SupportMessagesController < ApplicationController
   def destroy
     authorize @support_message
     @support_message.destroy
-    redirect_to support_message_path, notice: 'Support message destroyed successfully.'
+    redirect_to support_messages_path(slug: @support_message.project.slug), notice: 'Support message destroyed successfully.'
   end
   
   private
 
     def set_support_message
       @support_message = SupportMessage.find_by(id: params[:id])
+    end
+
+    def set_project
+      @project = Project.find_by(slug: params[:slug])
+    end
+
+    def is_user_project_creator
+      current_user == @project.user
     end
 
     def support_message_params
---
+++ test/system/support_messages_test.rb
@@ -0,0 +1,63 @@
+require 'application_system_test_case'
+
+class SupportMessageTest < ApplicationSystemTestCase
+  
+  setup do
+    @user = create(:user)
+    @non_creator = create(:user)
+    @project = create(:project, user: @user)
+    @subject = create(:support_message, project: @project)
+    visit support_messages_path(slug: @subject.project.slug)
+    sign_in(@user)
+  end
+
+  test 'show index page' do
+    assert_selector 'h1', text: 'Support Messages for project: ' + @subject.project.name
+  end
+
+  test 'create support message' do
+    click_on 'Add another'
+    fill_in 'support_message_order', with: 2
+    click_on 'Create Support message'
+    assert_text 'Support message created successfully.'
+  end
+
+  test 'edit support message' do
+    first('.edit-link').click
+    fill_in 'support_message_order', with: 3
+    click_on 'Update Support message'
+    assert_text 'Support message updated successfully.'
+  end
+
+  test 'destroy support message' do
+    first('.destroy-link').click
+    page.driver.browser.switch_to.alert.accept
+    assert_text 'Support message destroyed successfully.'
+  end
+
+  test 'non-creator cannot add message' do
+    sign_out
+    sign_in(@non_creator)
+    visit support_messages_path(slug: @subject.project.slug)
+    click_on 'Add another'
+    assert_text 'You do not have perission to create support messages.'
+  end
+
+  test 'non-creator cannot edit' do
+    sign_out
+    sign_in(@non_creator)
+    visit support_messages_path(slug: @subject.project.slug)
+    first('.edit-link').click
+    assert_text "Sorry, you don't have access to that"
+  end
+
+  test 'non-creator cannot delete' do
+    sign_out
+    sign_in(@non_creator)
+    visit support_messages_path(slug: @subject.project.slug)
+    first('.destroy-link').click
+    page.driver.browser.switch_to.alert.accept
+    assert_text "Sorry, you don't have access to that"
+  end
+  
+end
