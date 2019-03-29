28c1a036f8de8aa76f9d54d9d72379c11dba42a1
Richard Smith <richardsmith@dev.ngo>
2019-03-29 11:20:53 +0000

    formatting and freeze constants

--- app/models/support_message.rb
+++ app/models/support_message.rb
@@ -1,14 +1,14 @@
 
 
 class SupportMessage < ApplicationRecord
-  ALLOWED_STATUSES = ['Pending', 'Incomplete', 'Complete']
-  ALLOWED_OBJECTS = ['None', 'Unknown']
-  VALID_EVENT_TYPES = ['create', 'read', 'update', 'delete']
+  ALLOWED_STATUSES = ['Pending', 'Incomplete', 'Complete'].freeze
+  ALLOWED_OBJECTS = ['None', 'Unknown'].freeze
+  VALID_EVENT_TYPES = ['create', 'read', 'update', 'delete'].freeze
 
   belongs_to :project, class_name: 'Project'
-  validates :status, inclusion: { :in => ALLOWED_STATUSES }
+  validates :status, inclusion: { in: ALLOWED_STATUSES }
   validates :order, uniqueness: { scope: :project_id }
-  validates :rule_object_type, inclusion: { :in => ALLOWED_OBJECTS }
-  validates :rule_event_type, inclusion: { :in => VALID_EVENT_TYPES }
-  validates :rule_occurrences, numericality: { :greater_than_or_equal_to => 0 }
+  validates :rule_object_type, inclusion: { in: ALLOWED_OBJECTS }
+  validates :rule_event_type, inclusion: { in: VALID_EVENT_TYPES }
+  validates :rule_occurrences, numericality: { greater_than_or_equal_to: 0 }
 end
