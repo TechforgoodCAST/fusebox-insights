44fc0a549de25eb15c728f53555e7129b7676969
Ian Edwards <ianedwards559@gmail.com>
2019-04-10 14:30:57 +0100

    update policies to prevent add or edit for groups if user is not an admin

--- app/policies/group_policy.rb
+++ app/policies/group_policy.rb
@@ -1,9 +1,35 @@
 class GroupPolicy < ApplicationPolicy
+  def create?
+    if ProjectMember.where(project: record.project, user: user, role: "Admin").any?
+      true
+    else
+      user.id == record.project.user.id
+    end
+  end
+
+  def edit?
+    if ProjectMember.where(project: record.project, user: user, role: "Admin").any?
+      true
+    else
+      user.id == record.project.user.id
+    end
+  end
+
+
   def update?
-    user.id == record.author_id
+    if ProjectMember.where(project: record.project, user: user, role: "Admin").any?
+      true
+    else
+      user.id == record.project.user.id
+    end
+
   end
 
   def destroy?
-    user.id == record.author_id
+    if ProjectMember.where(project: record.project, user: user, role: "Admin").any?
+      true
+    else
+      user.id == record.project.user.id
+    end
   end
 end
