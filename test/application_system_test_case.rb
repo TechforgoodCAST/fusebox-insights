584f374839f82f1f78cc9feee55241597fc4f3e2
Andrew Sprinz <andrewsprinz@gmail.com>
2019-04-16 10:31:42 +0100

    Updating system tests

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -8,7 +8,7 @@ class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   def sign_in(user = @user)
     fill_in(:user_email, with: user.email)
     fill_in(:user_password, with: user.password)
-    click_button('Log in')
+    click_button('Sign in')
   end
 
   def sign_out
