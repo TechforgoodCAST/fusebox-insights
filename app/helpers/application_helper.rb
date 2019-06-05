805b57e485f082b5e24949a2bcbca39a631b598f
Suraj Vadgama <suninthesky@users.noreply.github.com>
2019-06-05 16:42:58 +0100

    Adjust margins

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,6 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
-  def current_action?(action)
-    action_name == action ? 'active' : nil
-  end
-
   def current_controller?(controller)
     controller_name == controller ? 'active' : nil
   end
