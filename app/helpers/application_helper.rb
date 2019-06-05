cda9aafca4d99b840d41f6cd54d8fd3bbfb037b6
Suraj Vadgama <suninthesky@users.noreply.github.com>
2019-06-05 13:34:31 +0100

    Add fields to support message form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
-  def current_controller?(controller)
-    controller_name == controller ? 'active' : nil
-  end
   def current_action?(action)
     action_name == action ? 'active' : nil
+  end
+
+  def current_controller?(controller)
+    controller_name == controller ? 'active' : nil
   end
 end
