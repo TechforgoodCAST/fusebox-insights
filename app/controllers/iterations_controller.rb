61baeacf3d18b4aac558de4b2c364297e94b8f05
Joseph Dudley <joseph@wearecast.org.uk>
2019-08-21 15:28:41 +0100

    Model improvements

--- app/controllers/iterations_controller.rb
+++ app/controllers/iterations_controller.rb
@@ -4,14 +4,22 @@ class IterationsController < ApplicationController
   def new
     @project = authorize Project.find(params[:project_id])
     @iteration = Iteration.new
-
-    5.times { @iteration.outcomes.build }
+    @iteration.start_date = Date.today
+    @iteration.outcomes.build
   end
 
   def create
     @project = authorize Project.find(params[:project_id])
-
-    @iteration = @project.iterations.create(iteration_params)
+    
+    if params[:commit]
+      draft = false;
+      status = 100
+    else
+      draft = true;
+      status = 0
+    end
+      
+    @iteration = @project.iterations.create(iteration_params.merge({:committing => !draft, :status => status}))
 
     if @iteration.save
       redirect_to project_iteration_url(@project, @iteration)
@@ -29,7 +37,7 @@ class IterationsController < ApplicationController
     @project = authorize Project.find(params[:project_id])
     @iteration = authorize Iteration.find(params[:id])
 
-    (5 - @iteration.outcomes.length).times { @iteration.outcomes.build }
+    @iteration.outcomes.build
   end
 
   def update
--- app/models/iteration.rb
+++ app/models/iteration.rb
@@ -4,11 +4,58 @@ class Iteration < ApplicationRecord
   belongs_to :project
 
   has_many :check_ins, dependent: :destroy
-  has_many :outcomes, dependent: :destroy
+  has_many :outcomes, inverse_of: :iteration, dependent: :destroy
+  
+  attr_accessor :committing
 
   accepts_nested_attributes_for :outcomes, reject_if: :all_blank, update_only: true, allow_destroy: true, limit: 5
 
-  enum status: { draft: -100, planned: 0, in_progress: 100, completed: 200 }, _prefix: :status
+  enum status: { draft: 0, committed: 100, completed: 200}, _prefix: :status
 
-  validates :title, :description, :start_date, :debrief_date, presence: true
+  validates :title, presence: true
+  validates :description, :start_date, :debrief_date, presence: true, :if => :committing
+  validates :outcomes, length: {minimum: 1, message: 'Should have at least 1 outcome defined.'}, :if => :committing
+  validate :start_date_cannot_be_in_the_past
+  validate :start_date_cannot_be_in_the_past
+  
+  def progress
+    
+    if status === 'committed'
+
+      if end_date < Date.today
+        progress = 'completed'
+      end
+
+      if start_date < Date.today && end_date > Date.today
+        progress = 'in_progress'
+      end
+
+      if start_date > Date.today
+        progress = 'planned'
+      end
+      
+    else
+      
+      progress = status
+      
+    end    
+    
+    return progress
+    
+  end
+  
+  private
+  
+  def start_date_cannot_be_in_the_past
+    if start_date.present? && start_date < Date.today
+      errors.add(:start_date, "Can't be in the past")
+    end
+  end
+    
+  def debrief_date_cannot_be_before_start_date
+    if debrief_date.present? && debrief_date < start_date
+      errors.add(:debrief_date, "Can't be before start date")
+    end
+  end
+  
 end
