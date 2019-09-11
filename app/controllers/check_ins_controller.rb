f7c54be9c47c9143d96019e3321e8fcd9ea4f0ee
Suraj Vadgama <suninthesky@users.noreply.github.com>
2019-09-11 17:30:27 +0100

    Remove check_in_complete method

--- app/controllers/check_ins_controller.rb
+++ app/controllers/check_ins_controller.rb
@@ -1,46 +1,44 @@
+# frozen_string_literal: true
+
 class CheckInsController < ApplicationController
-  
   def index
     new
   end
-  
+
   def new
     @project = Project.find(params[:project_id])
     @iteration = Iteration.find(params[:iteration_id])
     @check_in = authorize @iteration.check_ins.new
-    
+
     (@iteration.outcomes.length).times { @check_in.ratings.build }
   end
-  
+
   def create
     @project = Project.find(params[:project_id])
     @iteration = Iteration.find(params[:iteration_id])
-    
+
     @check_in = authorize @iteration.check_ins.create(check_in_params)
-    
+
     if @check_in.save
-      check_in_complete
+      NotificationsMailer.check_in_complete(@check_in, current_user).deliver_now
       redirect_to project_iteration_url(@project, @iteration)
     else
       render 'new'
     end
   end
-  
+
   def show
     @project = Project.find(params[:project_id])
     @iteration = Iteration.find(params[:iteration_id])
     @check_in = authorize CheckIn.find(params[:id])
   end
-  
+
   private
-    def check_in_params
-      params
-      .require(:check_in)
-      .permit(:notes, ratings_attributes: [:id, :score, :comments, :iteration, :outcome_id ])
-      .with_defaults(completed_by: current_user.id, complete_at: DateTime.now)
-    end
-  
-    def check_in_complete
-      NotificationsMailer.check_in_complete(@check_in, current_user).deliver_now
-    end
+
+  def check_in_params
+    params
+    .require(:check_in)
+    .permit(:notes, ratings_attributes: [:id, :score, :comments, :iteration, :outcome_id ])
+    .with_defaults(completed_by: current_user.id, complete_at: DateTime.now)
+  end
 end
