5b544beaed025dbd1451221beef1152a4d72d62e
Joseph Dudley <joseph@wearecast.org.uk>
2019-09-17 13:24:16 +0100

    Added TagHelper

--- test/helpers/check_ins_helper_test.rb
+++ test/helpers/check_ins_helper_test.rb
@@ -3,6 +3,7 @@
 require 'test_helper'
 
 class CheckInsHelperTest < ActionView::TestCase
+  include ActionView::Helpers::TagHelper
   setup do
     @on_track_rating = build(:rating, score: 0)
     @at_risk_rating = build(:rating, score: 100)
@@ -13,22 +14,22 @@ class CheckInsHelperTest < ActionView::TestCase
     #All off track
     @check_in = create(:check_in, ratings: [@on_track_rating, @at_risk_rating, @off_track_rating])
     
-    tag = makeSpan(200) + ' in some areas'
-    assert_equal(tag, render_check_in_status(@check_in))
+    span = makeSpan(200) + ' in some areas'
+    assert_equal(span, render_check_in_status(@check_in))
   end
   
   test "'check in status says 'for all areas' when ratings are not" do
     #All the same
     @check_in = create(:check_in, ratings: [@on_track_rating, @on_track_rating])
     
-    tag = makeSpan(0) + ' in all areas'
-    assert_equal(tag, render_check_in_status(@check_in))  
+    span = makeSpan(0) + ' in all areas'
+    assert_equal(span, render_check_in_status(@check_in))  
   end
   
   private
   
   def makeSpan(score)
-    '<span class="tag ' + Rating::SCORES[score].parameterize + '">' + Rating::SCORES[score] + '</span>'
+    tag.span(Rating::SCORES[score], class: 'tag ' + Rating::SCORES[score].parameterize)
   end
   
 end
