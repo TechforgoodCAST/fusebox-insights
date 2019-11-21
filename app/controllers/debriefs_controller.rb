1e86022ec2208930af5c78027b9d63647c50b4f9
Joseph Dudley <joseph@wearecast.org.uk>
2019-11-21 11:22:42 +0000

    Update app/controllers/debriefs_controller.rb
    Co-Authored-By: Suraj Vadgama <suninthesky@users.noreply.github.com>

--- app/controllers/debriefs_controller.rb
+++ app/controllers/debriefs_controller.rb
@@ -12,7 +12,7 @@ class DebriefsController < ApplicationController
 
     if Debrief.find_by(iteration: @iteration)
       flash[:alert] = "You've already debriefed this iteration"
-      redirect_to project_iteration_url(@project, @iteration) and return
+      return redirect_to project_iteration_url(@project, @iteration)
     end
 
     @debrief = authorize @iteration.build_debrief
