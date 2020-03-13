ec11f4f6b0017c44c99835009aeed8fb41cdbba5
Joseph Dudley <joseph@wearecast.org.uk>
2020-03-13 11:58:34 +0000

    Added support to subnav and fixed topics#index view

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -4,6 +4,13 @@ class OffersController < ApplicationController
   before_action :authenticate_user!, :load_project
 
   def show
-    @offer = Offer.find_by(id: params[:id])
+    @project = Project.find(params[:project_id])
+
+    if @project.topics.empty?
+      flash[:alert] = "Sorry, you don't have access to that"
+      redirect_back(fallback_location: root_path)
+    end
+
+    @offer = Offer.find(params[:id])
   end
 end
