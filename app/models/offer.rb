a8810f4c5ec8c9b6016bea069eac799067ac87e5
Joseph Dudley <joseph@wearecast.org.uk>
2020-03-18 11:25:25 +0000

    Renamed categories
    Per Sonya's suggestions

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -6,7 +6,7 @@ class Offer < ApplicationRecord
   has_and_belongs_to_many :topics
   has_many :support_requests
 
-  enum duration_category: { small: 0, medium: 1, large: 2 }
+  enum duration_category: { 'Get started': 0, 'Dive deeper': 1, 'Other': 2 }
 
   validates :title, :duration_category, :duration_description, :provider_email, presence: true
 end
